e707f40ac0533bf06e1d4208079e16bba8e88d50
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2025-02-14 16:58:06 -0300

    crud produtos

--- src/main/java/io/springboot/expert/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/io/springboot/expert/produtosapi/repository/ProdutoRepository.java
@@ -4,6 +4,9 @@ import io.springboot.expert.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    List<Produto> findByNome(String nome);
 }
